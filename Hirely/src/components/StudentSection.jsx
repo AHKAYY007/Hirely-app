338ec90fb7aa4d396d05789e1e28b0dfd2351eec
AHKAYY007 <iamakwaubokemeh@gmail.com>
2025-11-24 20:18:17 +0100

    add new images for old section and staff in StudentSection component

--- Hirely/src/components/StudentSection.jsx
+++ Hirely/src/components/StudentSection.jsx
@@ -11,23 +11,37 @@ import kid3 from "../assets/kids/kid3.jpeg";
 import kid4 from "../assets/kids/kid4.jpeg";
 import kid5 from "../assets/kids/kid5.jpeg";
 
+// old guys
+import old1 from "../assets/old/oldgizzer.webp";    
+import old2 from "../assets/old/Pflege.jpg";
+
+// staffs
+import staff1 from "../assets/staffs/staff1.jpg";
+import staff2 from "../assets/staffs/staff2.webp";
+import staff3 from "../assets/staffs/staff3.webp";
+
 const slides = [
-    { name: "kid1", src: kid1 },
-    { name: "kid2", src: kid2 },
-    { name: "kid3", src: kid3 },
-    { name: "kid4", src: kid4 },
-    { name: "kid5", src: kid5 },
+    { name: "", src: kid1 },
+    { name: "", src: old2 },
+    { name: "", src: staff1 },
+    { name: "", src: kid2 },
+    { name: "", src: kid3 },
+    { name: "", src: staff2 },
+    { name: "", src: old1 },
+    { name: "", src: kid4 },
+    { name: "", src: kid5 },
+    { name: "", src: staff3 }
 ];
 
 const StudentSection = () => {
     return (
         <div className="mt-20 mb-20">
-            <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mb-12 tracking-wide">
+            {/* <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mb-12 tracking-wide">
                 Our{" "}
                 <span className="bg-gradient-to-r from-green-500 to-teal-800 text-transparent bg-clip-text">
                     Students
                 </span>
-            </h2>
+            </h2> */}
             <Swiper
                 grabCursor={true}
                 centeredSlides={true}
@@ -77,7 +91,6 @@ const StudentSection = () => {
                     >
                         <div>
                             <h2>{slide.name}</h2>
-                            <a>explore</a>
                         </div>
                     </SwiperSlide>
                 ))}
